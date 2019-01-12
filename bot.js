aced3a6a2d52e1dc41d77afd33138ad6d8c9dec6
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-12 09:04:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'Hey') {
-    	message.reply('Heeey :ThomasFNielsenLove:');
+    	message.reply('Heeey :ThomasFNielsenLove: ');
   	}
 });
 
