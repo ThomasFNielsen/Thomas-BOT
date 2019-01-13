8e08dd1a5a5919810349a227f91a5e8af582a6bc
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-13 09:38:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,44 +5,6 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-// Initialize the invite cache
-const invites = {};
-
-// A pretty useful method to create a delay without blocking the whole script.
-const wait = require('util').promisify(setTimeout);
-
-client.on('ready', () => {
-    // "ready" isn't really ready. We need to wait a spell.
-    wait(1000);
-
-    // Load all invites for all guilds and save them to the cache.
-    client.guilds.forEach(g => {
-        g.fetchInvites().then(guildInvites => {
-            invites[g.id] = guildInvites;
-        });
-    });
-});
-
-client.on('guildMemberAdd', member => {
-    // To compare, we need to load the current invite list.
-    member.guild.fetchInvites().then(guildInvites => {
-        // This is the *existing* invites for the guild.
-        const ei = invites[member.guild.id];
-
-        // Update the cached invites
-        invites[member.guild.id] = guildInvites;
-
-        // Look through the invites, find the one for which the uses went up.
-        const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-
-        console.log(invite.code)
-
-        if (invite.code === "PBE7Qnc") {
-            return member.addRole(member.guild.roles.find(role => role.name === "MEDLEM"));
-        }
-    });
-});
-
 client.on('message', message => {
     if (message.content === 'Hey') {
     	message.channel.send(message.author + ' - Heeey, min ven!');
@@ -89,6 +51,44 @@ client.on('message', message => {
     if (message.content === 'godaften') {
     	message.channel.send(message.author + ' - Godaften, har du haft en god dag?');
   	    }
+});
+
+// Initialize the invite cache
+const invites = {};
+
+// A pretty useful method to create a delay without blocking the whole script.
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+    // "ready" isn't really ready. We need to wait a spell.
+    wait(1000);
+
+    // Load all invites for all guilds and save them to the cache.
+    client.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites => {
+            invites[g.id] = guildInvites;
+        });
+    });
+});
+
+client.on('guildMemberAdd', member => {
+    // To compare, we need to load the current invite list.
+    member.guild.fetchInvites().then(guildInvites => {
+        // This is the *existing* invites for the guild.
+        const ei = invites[member.guild.id];
+
+        // Update the cached invites
+        invites[member.guild.id] = guildInvites;
+
+        // Look through the invites, find the one for which the uses went up.
+        const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+
+        console.log(invite.code)
+
+        if (invite.code === "PBE7Qnc") {
+            return member.addRole(member.guild.roles.find(role => role.name === "MEDLEM"));
+        }
+    });
 });
 
 const newUsers = [];
