04c513b2da286b35af1a5379fe9d812a540e03a0
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-13 09:59:42 +0100

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,16 +1,7 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
 const newUsers = [];
 
 client.on("ready", () => {
   console.log("I am ready!");
-});
-
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -20,7 +11,7 @@ client.on("guildMemberAdd", (member) => {
 
   if (newUsers[guild.id].size > 10) {
     const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find(channel => channel.name === "nye-medlemmer").send("test\n" + userlist);
+    guild.channels.find(channel => channel.name === "chat").send("test\n" + userlist);
     newUsers[guild.id].clear();
   }
 });
