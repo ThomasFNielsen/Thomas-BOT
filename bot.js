b71b9c4602cd4d69cd1e1c238399d5cb42ba1866
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-13 09:23:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,29 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const newUsers = [];
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "nye-medlemmer").send("Velkommen til discorden!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
 client.on('ready', () => {
     console.log('I am ready!');
     
