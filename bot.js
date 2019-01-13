2c4531867223d356c8c3f68674cb03441a520d77
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-13 09:46:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,19 +91,7 @@ client.on('guildMemberAdd', member => {
     });
 });
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
 const newUsers = [];
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
 });
 
 client.on("guildMemberAdd", (member) => {
@@ -116,14 +104,6 @@ client.on("guildMemberAdd", (member) => {
     guild.channels.find(channel => channel.name === "nye-medlemmer").send("test123\n" + userlist);
     newUsers[guild.id].clear();
   }
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
-});
-
-client.login("NTMxMDk4MzgxNjQ1NTEyNzEz.DxyLvg.dIFmBaV2fZzlzNAzkaWmwHSyoiw");
 });
 
 // THIS  MUST  BE  THIS  WAY
