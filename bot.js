c6a837dc9bc5118fdb6e9e93f38e1c3e7b23a5d3
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-26 14:10:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,36 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
+client.on('guildMemberAdd', msg => { // Commands Go Inside The client.on('message', 
+msg => )
+msg.guild.channels.get('484648408372740099').send({embed: {
+color: 3447003,
+author: {
+  name: client.user.username,
+  icon_url: client.user.avatarURL
+},
+title: "Welcome To ()!",
+url: "https://districtservices.net",
+description: "@MEMBER",
+fields: [{
+    name: "Fields",
+    value: "They can have different fields with small headlines."
+  },
+  {
+    name: "Masked links",
+    value: "You can put [masked links](http://google.com) inside of rich embeds."
+  },
+  {
+    name: "Markdown",
+    value: "You can put all the *usual* **__Markdown__** inside of them."
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© Example"
+}
+
 client.on('message', message => {
     if (message.content === 'Hey') {
     	message.channel.send(message.author + ' - Heeey, min ven!');
