67ff574b2fb7a3ec30aff6cfc63fcce70b21d266
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-01-29 13:46:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,48 +8,49 @@ client.on("ready", () => {
 client.on('message', message => {
     if (message.content === 'Hey') {
     	message.channel.send(message.author + ' - Heeey, min ven!');
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'hey') {
     	message.channel.send(message.author + ' - Heeey, min ven!');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'Goddag') {
     	message.channel.send(message.author + ' - Goddag, min ven!');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'goddag') {
     	message.channel.send(message.author + ' - Goddag, min ven!');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'Godmorgen') {
     	message.channel.send(message.author + ' - Godmorgen, har du sovet godt?');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'godmorgen') {
     	message.channel.send(message.author + ' - Godmorgen, har du sovet godt?');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'Godaften') {
     	message.channel.send(message.author + ' - Godaften, har du haft en god dag?');
-  	    }
+    }
 });
 
 client.on('message', message => {
     if (message.content === 'godaften') {
     	message.channel.send(message.author + ' - Godaften, har du haft en god dag?');
-  	    }
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY 
