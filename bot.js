dc1e3d3db10e0fd36c9dbab53f3af32b60ab14b4
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-02-10 12:22:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,25 +42,6 @@ client.on('guildMemberAdd', member => {
     });
 });
 
-
-exports.run = (bot, message, args, func) => {
-
-    // Return Statements
-    if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires the Owner role**', 120000) // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
-    if (!args.join(" ") && args.join(" ").toUpperCase() !== 'NONE') return func.embed(message.channel, '**Please mention a channel**\n > *~setleave message*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
-
-    // Fetch the new channel they mentioned
-    let newMessage;
-    if (args.join(" ").toUpperCase() === 'NONE') newMessage = ''; // If they wrote the word none, it sets newMessage as empty.
-    else newMessage = args.join(" ").trim(); // If they didn't write none, set what they wrote as the message
-
-    // This will update the .text of the joinMessageDM_guildID object.
-    db.updateText(`leaveMessage_${message.guild.id}`, newMessage).then(i => {
-        func.embed(message.channel, `**Successfully updated welcome text to:**\n > *${args.join(" ").trim()}*`) // Finally, send in chat that they updated the channel.
-    })
-
-}
-
 client.on('message', message => {
     if (message.content === 'Hey') {
     	message.channel.send(message.author + ' - Heeey <:Love:537271965023731742>');
