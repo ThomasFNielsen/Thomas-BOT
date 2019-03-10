99ebe2322483e20ad0cdca0a595b166c2e9a3a9c
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2019-03-10 16:26:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 });
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('531141784600838144').send(member + " - Hjertelig velkommen til discorden <:Love:537271965023731742>"); 
+    member.guild.channels.get('531141784600838144').send(member + " - <:Hey:554322979182608404> & velkommen til discorden."); 
 });
 
 const wait = require('util').promisify(setTimeout);
