dc914456a68f4a871e0db0365a1a64c4945545b5
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2020-06-07 09:19:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,5 @@ client.on("ready", () => {
   console.log("Jeg er klar!");
 });
 
-client.on('message', (message) => {
-    if (message.content === '!move') {
-    	message.channel.send(message.author + ' would like to be moved to one of your rooms.');
-    }
-});
-
 // THIS  MUST  BE  THIS  WAY 
 client.login(process.env.BOT_TOKEN);
