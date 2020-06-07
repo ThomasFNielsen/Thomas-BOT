32e07d3513c1adae861311d114ea56f133230326
ThomasFNielsen <46403562+ThomasFNielsen@users.noreply.github.com>
2020-06-07 09:47:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,16 @@ client.on("ready", () => {
   console.log("Jeg er klar!");
 });
 
-bot.on('guildMemberAdd', member => {
-    member.guild.channels.get('531141784600838144').send("Welcome to the server! - Please read the #rules"); 
+client.on("guildMemberAdd", (MEMBER) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "new-member").send("Welcome to the server!\n" + userlist! - Please read the #rules");
+    newUsers[guild.id].clear();
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY 
